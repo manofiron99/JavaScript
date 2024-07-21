7687fa2dd203934564210e0ed993ce2daffa44ba
manofiron99 <kalitadip987@gmail.com>
2024-07-21 22:37:18 +0530

    second

--- Heart_trail_animation/script.js
+++ Heart_trail_animation/script.js
@@ -1,5 +1,7 @@
 const bodyE1 = document.querySelector("body")
 
 bodyE1.addEventListener("mousemove", (event)=>{
-    console.log("event.offsetY");
+    console.log("event.offsetX");
 })
+
+console.log("hi")
